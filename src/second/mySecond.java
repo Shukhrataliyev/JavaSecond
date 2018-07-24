d470048a84898e9a6e8623d0fd0d2265e47fc773
saliyev <saliyev@HSPS101-PC.hspaustin.org>
2018-07-23 23:53:07 -0500

    added another comment

--- src/second/mySecond.java
+++ src/second/mySecond.java
@@ -5,6 +5,7 @@ public class mySecond {
 	public static void main(String[] args) {
 		System.out.println("Football");
 		System.out.println("voleyball");
+		System.out.println("Basketball");
 	}
 
 }
