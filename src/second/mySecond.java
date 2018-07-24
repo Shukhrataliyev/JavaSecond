ef333cfad6a3e965230d7c174462e7bc833d3ba9
saliyev <saliyev@HSPS101-PC.hspaustin.org>
2018-07-24 00:11:13 -0500

    added loop statement

--- src/second/mySecond.java
+++ src/second/mySecond.java
@@ -1,11 +1,17 @@
 package second;
 
+import java.util.Iterator;
+
 public class mySecond {
 
 	public static void main(String[] args) {
 		System.out.println("Football");
 		System.out.println("voleyball");
 		System.out.println("Basketball");
+		
+		for (int i = 0; i < 10; i++) {
+		System.out.println(i);	
+		}
 	}
 
 }
